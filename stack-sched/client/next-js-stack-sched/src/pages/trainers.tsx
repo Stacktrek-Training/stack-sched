6e62e33b86dbfed320daefba4720dd0f036a62cc
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-04-14 15:32:59 +0800

    Fixed line spacing.

--- stack-sched/client/next-js-stack-sched/src/pages/trainers.tsx
+++ stack-sched/client/next-js-stack-sched/src/pages/trainers.tsx
@@ -12,7 +12,6 @@ import addIcon from '../assets/ico/ico-add.svg'
 const trainers = () => {
   return (
     <div>
-
       {/* navigation bar */}
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">

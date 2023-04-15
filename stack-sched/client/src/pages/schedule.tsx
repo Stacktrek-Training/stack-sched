40545c84d2289bbf5ded2423ba538c5fb912eeb2
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-04-15 13:15:59 +0800

    Updated trainers schedule page.

---
+++ stack-sched/client/src/pages/schedule.tsx
@@ -0,0 +1,366 @@
+import React from 'react'
+import Image from 'next/image';
+import logo from '../assets/logo/logo-stack-sched.png';
+import home from '../assets/ico/ico-home.png';
+import chat from '../assets/ico/ico-chat.png';
+import bell from '../assets/ico/ico-bell.png';
+import profile from '../assets/img/cyber-punk.jpeg'
+import exportIcon from '../assets/ico/ico-export.svg'
+import deleteIcon from '../assets/ico/ico-delete.svg'
+import addIcon from '../assets/ico/ico-add.svg'
+
+const schedule = () => {
+    return (
+        <div>
+            <nav className="bg-white border-gray-200 dark:bg-gray-900">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    {/* logo */}
+                    <a href="#/" className="flex items-center">
+                        <Image src={logo} alt="/" className='w-[21rem] ml-[-1rem]' />
+                    </a>
+
+                    {/* division for user menu (profile picture) */}
+                    <div className="flex items-center md:order-2">
+                        <button
+                            type="button"
+                            className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                            id="user-menu-button"
+                            aria-expanded="false"
+                            data-dropdown-toggle="user-dropdown"
+                            data-dropdown-placement="bottom"
+                        >
+                            <span className="sr-only">Open user menu</span>
+                            <Image src={profile} alt="user photo" className='w-8 h-8 rounded-full' />
+                        </button>
+
+                        {/* division for drop down menu when profile picture clicked */}
+                        <div
+                            className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                            id="user-dropdown"
+                        >
+
+                            {/* division for drop down details */}
+                            <div className="px-4 py-3">
+                                <span className="block text-sm text-gray-900 dark:text-white">
+                                    Bonnie Green
+                                </span>
+                                <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                                    name@flowbite.com
+                                </span>
+                            </div>
+                            {/* drop down links */}
+                            <ul className="py-2" aria-labelledby="user-menu-button">
+                                <li>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                    >
+                                        Dashboard
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                    >
+                                        Settings
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                    >
+                                        Earnings
+                                    </a>
+                                </li>
+                                <li>
+                                    <a
+                                        href="#"
+                                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                                    >
+                                        Sign out
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* side navigation button for mobile view */}
+                        <button
+                            data-collapse-toggle="mobile-menu-2"
+                            type="button"
+                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="mobile-menu-2"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                                className="w-6 h-6"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
+                        </button>
+                    </div>
+
+                    {/* division for link tabs */}
+                    <div
+                        className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 ml-[-1rem]"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col font-medium p-4 md:p-0 mt-5 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ml-0">
+                            <li>
+                                <a
+                                    href="/"
+                                    className="block py-2 pl-3 pr-4 text-white md:hover:text-blue-700 rounded md:bg-transparent md:text-black md:p-0 md:dark:text-blue-500 mt-1 mr-2"
+                                    aria-current="page"
+                                >
+                                    Dashboard
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-blue-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2"
+                                >
+                                    Trainers Schedule
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2"
+                                >
+                                    Events
+                                </a>
+                            </li>
+                            <li>
+                                <a href="/trainers"
+                                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2"
+                                >
+                                    Trainers
+                                </a>
+                            </li>
+                            {/* link icons */}
+                            <li>
+                                <a href="/">
+                                    <Image src={home} alt="/" className='w-[1.7rem] ml-12 mr-3' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <Image src={chat} alt="/" className='w-[1.7rem] mr-3' />
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <Image src={bell} alt="/" className='w-[1.5rem]' />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                {/* division for tabs and table */}
+        <div className='pr-5 pl-5 max-w-screen'>
+
+{/* division for search bar */}
+<div className='ml-4 mb-4'>
+  <div className="inline-flex w-[25rem] mt-5">
+    <label
+      htmlFor="search-dropdown"
+      className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+    >
+      Your Email
+    </label>
+    <button
+      id="dropdown-button"
+      data-dropdown-toggle="dropdown"
+      className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+      type="button"
+    >
+      Filter{" "}
+      <svg
+        aria-hidden="true"
+        className="w-4 h-4 ml-1"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </button>
+    <div
+      id="dropdown"
+      className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+    >
+      <ul
+        className="py-2 text-sm text-gray-700 dark:text-gray-200"
+        aria-labelledby="dropdown-button"
+      >
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Mockups
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Templates
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Design
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+          >
+            Logos
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div className="relative w-full">
+      <input
+        type="search"
+        id="search-dropdown"
+        className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-r-lg border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+        placeholder="Search"
+        required
+      />
+      <button
+        type="submit"
+        className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          aria-hidden="true"
+          className="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          ></path>
+        </svg>
+        <span className="sr-only">Search</span>
+      </button>
+    </div>
+  </div>
+
+  {/* division for header buttons */}
+  <div className="inline-flex rounded-md shadow-sm float-right mr-4 mt-4" role="group">
+    <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+      <Image src={exportIcon} alt="/" className='w-4 mr-2' />
+      Export
+    </button>
+    <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+      <Image src={deleteIcon} alt="/" className='w-4 mr-2' />
+      Delete
+    </button>
+    <button type="button" className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+      <Image src={addIcon} alt="/" className='w-4 mr-2' />
+      Add Shift
+    </button>
+  </div>
+</div>
+
+{/* division for trainers table */}
+<div className="relative shadow-md sm:rounded-lg mt-7 border-8 h-[40rem]">
+  <table className="bg-black-200 w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <thead className="text-xs text-gray-700 uppercase bg-gray-200 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" className="p-4">
+          <div className="flex items-center">
+            <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+            <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+          </div>
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Time
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Name
+        </th>
+        <th scope="col" className="px-6 py-3">
+          Status
+        </th>
+        <th scope="col" className="px-6 py-3 ">
+          Day
+        </th><th scope="col" className="px-6 py-3 ">
+          End Date
+        </th>
+        <th scope="col" className="px-6 py-3 ">
+          Action
+        </th>
+      </tr>
+    </thead>
+    <tbody className='overflow-auto'>
+      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+        <td className="w-4 p-4">
+          <div className="flex items-center">
+            <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+            <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+          </div>
+        </td>
+        <td className="px-6 py-4">
+          9:00 AM - 11:00 AM
+        </td>
+        <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+          <Image src={profile} alt="user profile" className='w-10 h-10 rounded-full' />
+          <div className="pl-3">
+            <div className="text-base font-semibold">Rie</div>
+            <div className="font-normal text-gray-500">Garfin, April Jane</div>
+          </div>
+        </th>
+        <td className="px-6 py-4">
+          <div className="flex items-center">
+            <div className="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div> Active
+          </div>
+        </td>
+        <td className="px-6 py-4">
+          M W F
+        </td>
+        <td className="px-6 py-4">
+          June 20, 2023
+        </td>
+        <td className="px-6 py-4">
+          <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit shift</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</div>
+            </nav >
+        </div>
+    )
+}
+
+export default schedule

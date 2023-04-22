aee67b25ff2e00e6cdf98b5549ce6b6e642474ef
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-04-22 16:25:04 +0800

    Added Buttons component for Shift and Trainers table.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,57 +3,9 @@ import 'flowbite'
 
 const Search = () => {
     return (
-        <div className="inline-flex w-full mt-5 mr-5">
-            <button
-                id="dropdown-button"
-                data-dropdown-toggle="dropdown"
-                className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 h-full"
-                type="button"
-            >
-                Filter{" "}
-                <svg
-                    aria-hidden="true"
-                    className="w-4 h-4 ml-1"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                        clip-rule="evenodd"
-                    ></path>
-                </svg>
-            </button>
-            {/* filter dropdown content */}
-            <div
-                id="dropdown"
-                className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-            >
-                <ul
-                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdown-button"
-                >
-                    <li>
-                        <button
-                            type="button"
-                            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                        >
-                            Person
-                        </button>
-                    </li>
-                    <li>
-                        <button
-                            type="button"
-                            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                        >
-                            Schedule Time
-                        </button>
-                    </li>
-                </ul>
-            </div>
+        <div>
             {/* search bar */}
-            <div className="relative w-96">
+            < div className="relative w-96" >
                 <input
                     type="search"
                     id="search-dropdown"
@@ -82,7 +34,7 @@ const Search = () => {
                     </svg>
                     <span className="sr-only">Search</span>
                 </button>
-            </div>
+            </div >
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,113 +5,113 @@ import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div className="h-screen max-w-screen">
-
+      {/* top navigation bar */}
       <Navbar />
 
-        {/* dashboard content */}
-        <div className="w-full min-h-screen bg-white dark:bg-gray-800 dark:border-gray-700">
-          <ul className="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
-            <li className="w-full hidden">
-              <button id="stats-tab" data-tabs-target="#stats" type="button" role="tab" aria-controls="stats" aria-selected="true" className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"></button>
-            </li>
-          </ul>
+      {/* dashboard content */}
+      <div className="w-full min-h-screen bg-white dark:bg-gray-800 dark:border-gray-700">
+        <ul className="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400" id="fullWidthTab" data-tabs-toggle="#fullWidthTabContent" role="tablist">
+          <li className="w-full hidden">
+            <button id="stats-tab" data-tabs-target="#stats" type="button" role="tab" aria-controls="stats" aria-selected="true" className="inline-block w-full p-4 rounded-tl-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"></button>
+          </li>
+        </ul>
 
-          {/* all cards */}
-          <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
+        {/* all cards */}
+        <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="stats" role="tabpanel" aria-labelledby="stats-tab">
 
-            {/* total graduates card */}
-            <div className="overflow-auto block max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-50 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 h-80 mb-4">
-              <table className=" dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full h-full">
-                <thead>
-                  <tr className="font-bold text-center text-xl text-gray-800"><th>Total Graduates</th></tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>
-                      {/* timeline */}
-                      <div className='flex mt-[-1rem]'>
-                        <ol className="items-center sm:flex">
-                          <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                              <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                                <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                              </div>
-                              <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+          {/* total graduates card */}
+          <div className="overflow-auto block max-w-m p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-50 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 h-80 mb-4">
+            <table className=" dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 w-full h-full">
+              <thead>
+                <tr className="font-bold text-center text-xl text-gray-800"><th>Total Graduates</th></tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>
+                    {/* timeline */}
+                    <div className='flex mt-[-1rem]'>
+                      <ol className="items-center sm:flex">
+                        <li className="relative mb-6 sm:mb-0">
+                          <div className="flex items-center">
+                            <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                              <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                             </div>
-                            <div className="mt-3 sm:pr-8">
-                              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Batch 10P</h3>
-                              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 15, 2023</time>
-                              <p className="text-base font-normal text-gray-500 dark:text-gray-400">Description</p>
+                            <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                          </div>
+                          <div className="mt-3 sm:pr-8">
+                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Batch 10P</h3>
+                            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 15, 2023</time>
+                            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Description</p>
+                          </div>
+                        </li>
+                        <li className="relative mb-6 sm:mb-0">
+                          <div className="flex items-center">
+                            <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                              <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                             </div>
-                          </li>
-                          <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                              <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                                <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                              </div>
-                              <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                            <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                          </div>
+                          <div className="mt-3 sm:pr-8">
+                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Batch 9P</h3>
+                            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 3, 2022</time>
+                            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Description</p>
+                          </div>
+                        </li>
+                        <li className="relative mb-6 sm:mb-0">
+                          <div className="flex items-center">
+                            <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
+                              <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                             </div>
-                            <div className="mt-3 sm:pr-8">
-                              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Batch 9P</h3>
-                              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">December 3, 2022</time>
-                              <p className="text-base font-normal text-gray-500 dark:text-gray-400">Description</p>
-                            </div>
-                          </li>
-                          <li className="relative mb-6 sm:mb-0">
-                            <div className="flex items-center">
-                              <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                                <svg aria-hidden="true" className="w-3 h-3 text-blue-800 dark:text-blue-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                              </div>
-                              <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                            </div>
-                            <div className="mt-3 sm:pr-8">
-                              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Batch 8P</h3>
-                              <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 28, 2022</time>
-                              <p className="text-base font-normal text-gray-500 dark:text-gray-400">Description</p>
-                            </div>
-                          </li>
-                        </ol>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            {/* sub cards */}
-            <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
-              {/* active trainers card */}
-              <div className="flex flex-col items-center justify-center">
-                <dt className=" text-4xl font-extrabold text-[#EF8134]">19</dt>
-                <dd className="text-gray-500 dark:text-gray-400">Active Trainers</dd>
-              </div>
-              {/* for pooling trainers card */}
-              <div className="flex flex-col items-center justify-center">
-                <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">8</dt>
-                <dd className="text-center text-gray-500 dark:text-gray-400">For Pooling Trainers</dd>
-              </div>
-              {/* ongoing class card */}
-              <div className="flex flex-col items-center justify-center">
-                <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">12</dt>
-                <dd className="text-gray-500 dark:text-gray-400">Ongoing Class</dd>
-              </div>
-              {/* pending class card */}
-              <div className="flex flex-col items-center justify-center">
-                <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">1</dt>
-                <dd className="text-gray-500 dark:text-gray-400">Pending Class</dd>
-              </div>
-              {/* graduated class card */}
-              <div className="flex flex-col items-center justify-center">
-                <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">--</dt>
-                <dd className="text-gray-500 dark:text-gray-400">Graduated Class</dd>
-              </div>
-              {/* active students card */}
-              <div className="flex flex-col items-center justify-center">
-                <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">143</dt>
-                <dd className="text-gray-500 dark:text-gray-400">Active Students</dd>
-              </div>
-            </dl>
+                            <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
+                          </div>
+                          <div className="mt-3 sm:pr-8">
+                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Batch 8P</h3>
+                            <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 28, 2022</time>
+                            <p className="text-base font-normal text-gray-500 dark:text-gray-400">Description</p>
+                          </div>
+                        </li>
+                      </ol>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+          {/* sub cards */}
+          <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-6 dark:text-white sm:p-8">
+            {/* active trainers card */}
+            <div className="flex flex-col items-center justify-center">
+              <dt className=" text-4xl font-extrabold text-[#EF8134]">19</dt>
+              <dd className="text-gray-500 dark:text-gray-400">Active Trainers</dd>
+            </div>
+            {/* for pooling trainers card */}
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">8</dt>
+              <dd className="text-center text-gray-500 dark:text-gray-400">For Pooling Trainers</dd>
+            </div>
+            {/* ongoing class card */}
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">12</dt>
+              <dd className="text-gray-500 dark:text-gray-400">Ongoing Class</dd>
+            </div>
+            {/* pending class card */}
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">1</dt>
+              <dd className="text-gray-500 dark:text-gray-400">Pending Class</dd>
+            </div>
+            {/* graduated class card */}
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">--</dt>
+              <dd className="text-gray-500 dark:text-gray-400">Graduated Class</dd>
+            </div>
+            {/* active students card */}
+            <div className="flex flex-col items-center justify-center">
+              <dt className="mb-2 text-4xl font-extrabold text-[#EF8134]">143</dt>
+              <dd className="text-gray-500 dark:text-gray-400">Active Students</dd>
+            </div>
+          </dl>
         </div>
-        </div>
+      </div>
+    </div>
   )
 }

8e0564ca1d8864a7332cac9e92361db2050d4621
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-04-25 13:44:03 +0800

    Fixed routing to dashboard.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,9 @@ const Navbar = () => {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 {/* logo */}
-                <Link href="/" className="flex items-center">
+                <a href="/" className="flex items-center">
                     <Image src={logo} alt="/" className="w-[16rem] ml-[-1rem]" />
-                </Link>
+                </a>
 
                 {/* profile picture */}
                 <div className="flex items-center md:order-2">
@@ -95,9 +95,9 @@ const Navbar = () => {
                 >
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-5 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ml-[-0.1rem]">
                         <li>
-                            <Link href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded md:bg-transparent md:p-0 md:hover:text-blue-700 md:dark:text-blue-500 mt-1 mr-2">
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded md:bg-transparent md:p-0 md:hover:text-blue-700 md:dark:text-blue-500 mt-1 mr-2">
                                 Dashboard
-                            </Link>
+                            </a>
                         </li>
                         <li>
                             <Link href="/schedule" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2">
@@ -116,9 +116,9 @@ const Navbar = () => {
                         </li>
                         {/* link icons */}
                         <li>
-                            <Link href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2 ml-[-2.9rem]">
+                            <a href="/" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2 ml-[-2.9rem]">
                                 <Image src={home} alt="/" className="w-[1.6rem] ml-12 mr-3" />
-                            </Link>
+                            </a>
                         </li>
                         <li>
                             <Link href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 mt-1 mr-2">

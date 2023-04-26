68f912a38630640cc83f240bd9e4f0930302657f
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-04-26 13:29:36 +0800

    Changed input type for mobile number field.

--- src/pages/trainers.tsx
+++ src/pages/trainers.tsx
@@ -301,7 +301,7 @@ const trainers = () => {
                         </div>
                         {/* mobile number input */}
                         <div>
-                          <input type="tel" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mobile number" pattern="[0-9]{11}" required />
+                          <input type="number" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Mobile number" pattern="[0-9]{11}" required />
                         </div>
                       </div>
                       <div>

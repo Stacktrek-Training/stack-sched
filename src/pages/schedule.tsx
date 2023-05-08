82379e2b9273bb673ef1757ea714bfafa98852e7
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-08 14:21:55 +0800

    Updated dropdown button

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -425,7 +425,7 @@ const schedule = () => {
                     {/* trainers dropdown button */}
                     <div className='relative'>
                       <label className="mb-4 text-sm font-medium text-gray-900 dark:text-white">Select Trainers </label>
-                      <button id="dropdownBgHoverButton" data-dropdown-toggle="dropdownBgHover" className="mt-1 text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center  items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full flex justify-between" type="button">
+                      <button id="dropdownBgHoverButton" data-dropdown-toggle="dropdownBgHover-edit" className="mt-1 text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center  items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 w-full flex justify-between" type="button">
                         Trainers
                         <svg className="w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
@@ -433,7 +433,7 @@ const schedule = () => {
                       </button>
 
                       {/* trainers dropdown content */}
-                      <div id="dropdownBgHover" className="items-center z-10 hidden bg-white rounded-lg shadow dark:bg-gray-700 w-full">
+                      <div id="dropdownBgHover-edit" className="items-center z-10 hidden bg-white rounded-lg shadow dark:bg-gray-700 w-full">
                         <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownBgHoverButton">
                           <li>
                             <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
@@ -450,8 +450,8 @@ const schedule = () => {
                       <label className="mb-4 text-sm font-medium text-gray-900 dark:text-white">Repeat every </label>
                       <div className='mt-1'>
                         <input className="w-20 text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 border-none ml-[0.01rem] dark:focus:ring-blue-800" type="number" placeholder='1' />
-                        <button id="dropdownRepeatButton" data-dropdown-toggle="dropdownRepeat" className="ml-4 text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">week<svg className="w-4 h-4 ml-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-                        <div id="dropdownRepeat" className="border z-10 hidden bg-white rounded-lg shadow dark:bg-gray-700 w-28">
+                        <button id="dropdownRepeatButton" data-dropdown-toggle="dropdownRepeat-edit" className="ml-4 text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">week<svg className="w-4 h-4 ml-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+                        <div id="dropdownRepeat-edit" className="border z-10 hidden bg-white rounded-lg shadow dark:bg-gray-700 w-28">
                           <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownBgHoverButton">
                             {/* day */}
                             <li>
@@ -586,11 +586,8 @@ const schedule = () => {
                 </form>
               </div>
             </div>
-
           </div>
-
         </div>
-
       </div>
     </div>
   )

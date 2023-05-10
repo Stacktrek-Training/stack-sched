f99d337f387dd7784031198ae14472cb6a908b61
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-05-10 12:47:23 +0800

    fix: changed label for shift time and days in add form

--- src/pages/trainers.tsx
+++ src/pages/trainers.tsx
@@ -394,16 +394,16 @@ const trainers = () => {
                                 <input type="date" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                               </div>
 
-                              {/* available time */}
+                              {/* shift time */}
                               <div>
-                                <label className="mb-4 text-sm font-medium text-gray-900 dark:text-white">Available Time</label>
+                                <label className="mb-4 text-sm font-medium text-gray-900 dark:text-white">Shift Time</label>
                                 <input className="mt-1 w-full border border-gray-300 text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center items-center inline-flex dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="time" />
                               </div>
                             </div>
 
-                            {/* available days */}
+                            {/* shift days */}
                             <div>
-                              <label className="mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Available Days</label>
+                              <label className="mb-2 text-sm font-medium text-gray-900 dark:text-white">Shift Days</label>
                               <ul className="mt-1 items-center w-full text-sm font-medium text-gray-900  rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                                 {/* monday */}
                                 <li className="w-full  dark:border-gray-600">
